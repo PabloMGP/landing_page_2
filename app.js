5789a43815f359071bcb66cd34bc568efa5f09dd
Pablo <pgarciacardone@gmail.com>
2023-04-12 00:10:16 +0100

    Add profile picture, brand logo, styling

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 window.addEventListener("DOMContentLoaded", () => {
-
 });
 
 const toggleMenu = document.querySelector(".toggle-navbar");
 const mainContainer = document.querySelector(".main-container");
 
+// Toggle menu 
 toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("active");
     mainContainer.classList.toggle("active");
+    // Disable toggle menu for Fold and similar devices
     if (window.matchMedia("(max-width: 300px)").matches) {
         toggleMenu.classList.remove("active");
         mainContainer.classList.remove("active");
